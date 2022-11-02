3c3fe80e7d2e84809d1cee3090507b33b175be3e
montexbjeliseo <secondmtx@gmail.com>
2022-11-01 21:28:43 -0300

    Project created

---
+++ src/test/java/arg/programa/mtx/argservr/ArgServrApplicationTests.java
@@ -0,0 +1,13 @@
+package arg.programa.mtx.argservr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgServrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
