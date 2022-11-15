c9fe95ef4eca8fb7136071c7e569aeccc819f594
montexbjeliseo <secondmtx@gmail.com>
2022-11-15 18:13:25 -0300

    Backlog: 3 userstories added

--- src/main/java/com/mtx/argservr/ArgServrApplication.java
+++ src/main/java/com/mtx/argservr/ArgServrApplication.java
@@ -1,4 +1,4 @@
-package arg.programa.mtx.argservr;
+package com.mtx.argservr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
