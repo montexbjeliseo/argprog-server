c80dbdec9b357543792fcaa8b0b464bbff51d6c2
montexbjeliseo <secondmtx@gmail.com>
2023-04-23 19:18:23 -0300

    -deploy 0005

--- target/generated-sources/annotations/com/mtx/argservr/mapper/PublicResourceMapperImpl.java
+++ target/generated-sources/annotations/com/mtx/argservr/mapper/PublicResourceMapperImpl.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-04-23T19:00:08-0300",
+    date = "2023-04-23T19:11:25-0300",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.19 (Microsoft)"
 )
 @Component
