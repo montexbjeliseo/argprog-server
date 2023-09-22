d097798907b71503b22cf43b0b5cea29eb13b4f7
montexbjeliseo <secondmtx@gmail.com>
2023-09-22 16:17:16 -0300

    docs: allow jwt

--- src/main/java/com/mtx/argservr/config/SwaggerConfig.java
+++ src/main/java/com/mtx/argservr/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.mtx.argservr.config;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.common.collect.Lists;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,13 +13,19 @@ import com.google.common.base.Predicates;
 import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
+    public static final String DEFAULT_INCLUDE_PATTERN = "/api/.*";
 
     @Bean
     public Docket api() {
@@ -28,9 +35,32 @@ public class SwaggerConfig {
         predicateList.add(RequestHandlerSelectors.basePackage("com.mtx.argservr.security.controller"));
 
         return new Docket(DocumentationType.SWAGGER_2)
+                .securityContexts(Lists.newArrayList(securityContext()))
+                .securitySchemes(Lists.newArrayList(apiKey()))
                 .select()
                 .apis(Predicates.or(predicateList))
                 .paths(PathSelectors.any())
                 .build();
     }
+
+    private ApiKey apiKey(){
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex(DEFAULT_INCLUDE_PATTERN))
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Lists.newArrayList(
+                new SecurityReference("JWT", authorizationScopes));
+    }
+
 }
